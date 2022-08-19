e81ef755162ce35592eb5a7fed96ae9f606bb59b
abrhamgg <abrhamgg@gmail.com>
2022-08-19 23:46:12 +0300

    fix

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
@@ -69,8 +69,8 @@ if __name__ == '__main__':
         print("User.id should be unique")
 
     u_pwd = "myPassword"
-    user_1.__password = u_pwd
-    if user_1.__password == u_pwd:
+    user_1.password = u_pwd
+    if user_1.password == u_pwd:
         print("User.password should be hashed")
 
     if user_2.password is not None:
@@ -80,8 +80,8 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to None")
 
-    user_2.__password = 89
-    if user_2.__password is not None:
+    user_2.password = 89
+    if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
     if not user_1.is_valid_password(u_pwd):
