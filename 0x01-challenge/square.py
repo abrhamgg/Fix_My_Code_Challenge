134435b13e7bc86c8804e5e515c361e72c8df93a
abrhamgg <abrhamgg@gmail.com>
2022-10-11 21:21:34 +0300

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""defines a square class"""
 
 
 class square():
-    """defines a class for square"""
+
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """check and sets the value"""
         for key, value in kwargs.items():
-            if value < 0:
-                raise ValueError("Value can't be negative")
-            if key in ['width', 'height']:
-                setattr(self, key, value)
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """ Permineter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -28,6 +22,7 @@ class square():
 
 
 if __name__ == "__main__":
+
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
